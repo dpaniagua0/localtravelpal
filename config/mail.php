b3ab77372d3e3cdcf41f16c58fb38bad7941daee
dpaniagua0 <dpaniagua0@gmail.com>
2016-06-03 13:19:06 -0500

    email password reset #2

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@localtravelpal.com', 'name' => 'No reply'],
 
     /*
     |--------------------------------------------------------------------------

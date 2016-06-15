212e3824f184a5aced2db24badd97def907c229f
dpaniagua0 <dpaniagua0@gmail.com>
2016-06-15 11:14:16 -0500

    destination view #3

--- resources/views/destinations/show.blade.php
+++ resources/views/destinations/show.blade.php
@@ -1,10 +1,41 @@
 @extends('layouts.app')
 @section('page-title','Destination')
 @section('content')
-<div class="cover">
-  <img src="http://placehold.it/1200x450" style="width: 100%">
+<style>
+  .destination-bg {
+    background: url('http://placehold.it/1200x350') no-repeat;
+    background-position: center center;
+    background-size: cover; 
+  }
+</style>
+<div class="section-top destination-bg">
+  <div class="has-pull-top"></div>
 </div>
-{{ $destination}}
+<div class="section pull-top">
+  <div class="destination-details">
+    <div class="destination-title">
+      <h1>{{ $destination->title }} <small>with</small> {{$destination->owner->name}}</h1>
+      <h3>{{ $destination->location }}</h3>
+      @if(count($destination->categories) > 0)
+        @foreach($destination->categories as $category)
+          <a href="#">#{{$category->name}}</a>
+        @endforeach
+      @endif
+    </div>
+    
+    <div class="destination-actions">
+        <a class="btn btn-danger contact-btn">Contact {{ $destination->owner->name}}</a>
+        <a class="btn btn-danger contact-btn">Add to wishlist</a>
+    </div>
+    <div class="destination-description">
+      {{ $destination->description}}
+    </div>
+  </div>
+
+  <div class="destination-banner">
+    <img src="{{asset($destination->owner->img_path)}}" style="width: 100%">
+  </div>
+</div>
 
 @endsection
 
